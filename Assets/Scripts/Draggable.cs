6f7e48c47275fb88652df2d69d9954e285881ef1
Maxime Burri <maxime.burri@gmail.com>
2017-04-19 17:26:24 +0200

    Add collision with trash
    Destroy the waste

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -1,8 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 [RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(Rigidbody))]
 
+
+// Draggable object, when collising with another object, destroy
 public class Draggable : MonoBehaviour {
+	
+	GameObject objectTrigerred = null;
+
 	Vector3 screenPoint;
 	Vector3 offset;
 
@@ -17,6 +23,25 @@ public class Draggable : MonoBehaviour {
 		Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
 		Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
 		transform.position = cursorPosition;
+	}
+
+	void OnMouseUp(){
+		if (isObjectInTrash ())
+			Destroy (gameObject);
+	}
+
+	void OnTriggerEnter (Collider hit) {
+		Debug.Log( "OnTriggerEnter" );
+		objectTrigerred = hit.gameObject;
+	}
+
+	void OnTriggerExit (Collider hit) {
+		Debug.Log( "OnTriggerExit" );
+		objectTrigerred = null;
+	}
+
+	bool isObjectInTrash() {
+		return objectTrigerred != null;
 	}
 }
 
