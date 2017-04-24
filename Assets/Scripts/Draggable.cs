3bbd4687fd918a71914c790367c0256432077ce9
Maxime Burri <maxime.burri@gmail.com>
2017-04-24 17:13:10 +0200

    Add mutliples trashes

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -7,44 +7,64 @@ using System.Collections;
 // Draggable object, when collising with another object, destroy
 public class Draggable : MonoBehaviour {
 	
-	GameObject objectTrigerred = null;
+	Trash trash = null;
 
 	Vector3 screenPoint;
 	Vector3 offset;
 
 	void OnMouseDown(){
-		Debug.Log ("OnMouseDown");
 		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
 		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
 	}
 
 	void OnMouseDrag(){
-		Debug.Log ("OnMouseDrag");
 		Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
 		Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
 		transform.position = cursorPosition;
 	}
 
 	void OnMouseUp(){
-		if (isObjectInTrash ())
+		if (isObjectInTrash ()) {
+			trash.Close ();
 			Destroy (gameObject);
+		}
 	}
 
 	void OnTriggerEnter (Collider hit) {
-		Debug.Log( "OnTriggerEnter" );
+		Trash hitTrash = hit.gameObject.GetComponent<Trash> ();
 
-		// Test if it's trash
-		if(hit.gameObject.GetComponent<Trash>())
-			objectTrigerred = hit.gameObject;
+		if (hitTrash) {
+			// Already a trash, close it
+			if(trash)
+				trash.Close ();
+			trash = hitTrash;
+			trash.Open ();
+		}
+	}
+
+	void OnTriggerStay (Collider hit) {
+		Trash hitTrash = hit.gameObject.GetComponent<Trash> ();
+
+		// If no other trash, it's the current trash
+		if (trash == null) {
+			hitTrash.Open ();
+			trash = hitTrash;
+		}
 	}
 
 	void OnTriggerExit (Collider hit) {
-		Debug.Log( "OnTriggerExit" );
-		objectTrigerred = null;
+		Trash trashExit = hit.gameObject.GetComponent<Trash> ();
+
+		if (trashExit) {
+			trashExit.Close ();
+
+			if (trashExit == trash)
+				trash = null;
+		}
 	}
 
 	bool isObjectInTrash() {
-		return objectTrigerred != null;
+		return trash != null;
 	}
 }
 
--- Assets/Trash.cs
+++ Assets/Trash.cs
@@ -2,5 +2,26 @@
 using System.Collections;
 
 public class Trash : MonoBehaviour {
+	public string trashType = "waste";
 
+	Sprite spriteTrashClose;
+	Sprite spriteTrashOpen;
+
+	public enum Direction {North, East, South, West};
+
+	public Direction test;
+	void Awake(){
+		spriteTrashClose = Resources.Load<Sprite> 
+			("Sprites/Trash/"+trashType+"/"+trashType+"_close");
+		spriteTrashOpen = Resources.Load<Sprite> 
+			("Sprites/Trash/"+trashType+"/"+trashType+"_open");
+	}
+
+	public void Open(){
+		GetComponent<SpriteRenderer> ().sprite = spriteTrashOpen;
+	}
+
+	public void Close(){
+		GetComponent<SpriteRenderer> ().sprite = spriteTrashClose;
+	}
 }
