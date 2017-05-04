48763ec2e51034cf4d84107b0cd88c14b78d95d5
cicciu <salvatore.cicciu@gmail.com>
2017-05-04 20:27:38 +0200

    add level scene

---
+++ Assets/Resources/Scripts/ButtonMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonMenu : MonoBehaviour {
+    public Image imgLvl;
+    public Text txtLvl;
+    // Use this for initialization
+    void Start () {
+		
+    }
+	
+    // Update is called once per frame
+    void Update () {
+		
+    }
+
+    public void LoadScene(string sceneName)
+    {
+
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ChangeLevel(int numLevel)
+    {
+        imgLvl.sprite = Resources.Load<Sprite>("Sprites/Background/level"+numLevel);
+        txtLvl.text = "Level " + numLevel;
+    }
+}
