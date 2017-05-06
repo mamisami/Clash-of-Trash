4feb11563b1d84ed9be6f00de734259a8a78b736
maxime <maxime.burri@gmail.com>
2017-05-06 17:34:32 +0200

    Add text when put in trash

--- Assets/Resources/Scripts/Trash.cs
+++ Assets/Resources/Scripts/Trash.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class Trash : MonoBehaviour {
 	public string trashType = "waste";
@@ -8,6 +9,7 @@ public class Trash : MonoBehaviour {
 	Sprite spriteTrashClose;
 	Sprite spriteTrashOpen;
 	GameObject particleEffect;
+	GameObject bonusText;
 
 	void Awake(){
 		spriteTrashClose = Resources.Load<Sprite> 
@@ -16,6 +18,8 @@ public class Trash : MonoBehaviour {
 			("Sprites/Trash/"+trashType+"/"+trashType+"_open");
 		particleEffect = Resources.Load<GameObject>
 			("Trash/ParticleSystemTrash");
+		bonusText = Resources.Load<GameObject>
+			("Trash/PopText3D");
 	}
 
 	public void Open(){
@@ -29,10 +33,23 @@ public class Trash : MonoBehaviour {
 	public void MakeParticleEffect(){
 		if (this.particleEffect) {
 			Vector3 posParticle = transform.position;
-			posParticle.y += 1;
+			posParticle.y += 1.5f;
 			posParticle.z += 1;
 			GameObject particleClone = Instantiate (particleEffect, posParticle, Quaternion.Euler(new Vector3(-90, 0, 0))) as GameObject;
-			Destroy (particleClone, 2); 
+			Destroy (particleClone, 2);
 		}
+		MakePopText ();
+	}
+
+	public void MakePopText(){
+		Vector3 posText = transform.position;
+		GameObject bonusClone = Instantiate (bonusText, posText, Quaternion.identity) as GameObject;
+		Destroy (bonusClone, 3f); // Destoy 3 seconds after
+		TextMesh txtMesh = bonusClone.GetComponentInChildren<TextMesh> ();
+		txtMesh.alignment = TextAlignment.Center;
+		txtMesh.text = "+1";
+		posText.y += 1f;
+		//posText.z += 1;
+		bonusClone.transform.position = posText;
 	}
 }
