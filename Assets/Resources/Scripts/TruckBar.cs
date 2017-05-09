172e8477adda2491510ab73d61a6416ffde65451
Maxime Burri <maxime.burri@gmail.com>
2017-05-09 15:48:04 +0200

    Remove unreachable code

--- Assets/Resources/Scripts/TruckBar.cs
+++ Assets/Resources/Scripts/TruckBar.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class TruckBar : MonoBehaviour {
 
 	float positionX = 0;
-	Vector3 scale;
 
 	GameObject truck;
 	GameObject emptyTrash1;
@@ -33,22 +32,18 @@ public class TruckBar : MonoBehaviour {
 		emptyTrashScale = emptyTrash1.transform.lossyScale;
 
 		positionX = truck.transform.position.x;
-		scale = transform.localScale;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//iTween.MoveTo(gameObject,new Vector3(12.5f,-56,90),60);
 		iTween.MoveTo(truck,  iTween.Hash("x", positionX+1.6f,"time",2,"easetype", iTween.EaseType.linear));
 	}
 
 	public void Open() {
-		//iTween.ScaleTo(gameObject,  iTween.Hash("scale", 1.1F*scale,"time",0.8f,"easetype", iTween.EaseType.easeOutExpo));
 		iTween.FadeTo(gameObject,  iTween.Hash("alpha", 0.5f,"time",0.8f,"easetype", iTween.EaseType.easeOutExpo));
 	}
 
 	public void Close() {
-		//iTween.ScaleTo(gameObject,  iTween.Hash("scale", scale,"time",0.8f,"easetype", iTween.EaseType.easeOutExpo));
 		iTween.FadeTo(gameObject,  iTween.Hash("alpha", 1f,"time",0.8f,"easetype", iTween.EaseType.easeOutExpo));
 	}
 
@@ -58,11 +53,9 @@ public class TruckBar : MonoBehaviour {
 		if (num == 0)
 			num = indexTrash % 2 + 1;
 		
-		GameObject emptyTrash = emptyTrash1;
 		Vector3 pos = emptyTrash1Position;
 
 		if (num == 2) {
-			emptyTrash = emptyTrash2;
 			pos = emptyTrash2Position;
 
 			// Update current trash 1
